dad50594e7718c250e5e40074e06c0d6d83a1c9f
Someidiotter <anullowo@gmail.com>
2024-03-29 13:06:20 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Arrays_Null_Asher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
